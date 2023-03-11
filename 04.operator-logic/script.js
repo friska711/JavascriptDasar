7d657195f42d36e4ed3406fd7fefdecc9ce842bb
friska711 <126258596+friska711@users.noreply.github.com>
2023-03-11 09:24:17 +0700

    Update script.js

--- 04.operator-logic/script.js
+++ 04.operator-logic/script.js
@@ -6,20 +6,20 @@ const nilaiBahasa = 60;
 const lulusKedokteran= nilaiIPA > 70 && nilaiMatematika > 70 
                           && nilaiIPS > 70 && nilaiBahasa > 70
 
-console.log('lulusKedokteran =', lulusKedokteran)
+console.log(`lulusKedokteran =`, lulusKedokteran)
 
 const lulusInformatika = nilaiIPA > 70 || nilaiIPS > 70 
                          || nilaiBahasa >70 && nilaiMatematika > 80
 
-console.log('lulusInformatika =', lulusInformatika)
+console.log(`lulusInformatika =`, lulusInformatika)
 
 const lulusSekolah = (nilaiIPA + nilaiIPS + nilaiBahasa + nilaiMatematika) / 4 > 70
 
-console.log('lulusSekolah =', lulusSekolah)
+console.log(`lulusSekolah =`, lulusSekolah)
 
 const lulusKeperawatan = !lulusKedokteran && lulusSekolah
 
-console.log('lulusKeperawatan =', lulusKeperawatan)
+console.log(`lulusKeperawatan =`, lulusKeperawatan)
 
 console.log((lulusKeperawatan && lulusSekolah) || (lulusSekolah && lulusKedokteran));
 
